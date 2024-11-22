525dda64926990ad61a86afdf19119d980ac1b1e
20gaurav04 <btechug2026@gmail.com>
2024-11-22 09:47:26 +0530

    user-form

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserForm from './components/UserForm';
+const App = () => {
+ return (
+ <div>
+ <h1>Insert Record in MongoDB Atlas</h1>
+ <UserForm />
+ </div>
+ );
+};
+export default App;
